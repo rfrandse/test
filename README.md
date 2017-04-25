e0ec449178c7775f831c2e8ec2680a5c1655aaa5
Reed Frandsen <rfrandse@us.ibm.com>
2017-04-25 13:11:28 -0500

    gerrit-report2 version 1
    Signed-off-by: Reed Frandsen <rfrandse@us.ibm.com>

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# gerrit-report2
-Queries Gerrit for stale commits and assigns follow-up actions. 
+# Purpose
+
+Generate information on next-steps for outstanding Gerrit commits per
+the OpenBMC merge policies.
+This script originated from
+https://github.com/williamspatrick/gerrit-report
+and converted to run in python2 
+Additionally reports are sent via slack to openbmc team members
+
+# Requirements
+
+1. python2
+2. An ssh alias to Gerrit `openbmc.gerrit`
+3. https://github.com/os/slacker module installed. 
+4. config.py file with 
+    #!/usr/bin/python
+    token = "<insert slack token here>"
+
+# Usages
+
+## Default use
+
+Generate a report for all commits untouched in the last day:
+
+```
+gerrit-report.py --protocol slack report
+gerrit-report.py --protocol irc report
+```
+
+## Individual developer
+
+List the current status on all your own commits.
+
+```gerrit-report.py --age=0d --owner=<github_id> report```
+
+## Team lead
+
+List the current status of your teams commits.
+
+```gerrit-report.py --age=0d --owner=<github0> --owner=<github1> ... report```
+
+This has the effect of a Gerrit query such as
+`(owner:github0 OR owner:github1)`.
+
---
+++ gerrit-report2.py
@@ -0,0 +1,287 @@
+#!/usr/bin/python
+
+
+####!/usr/bin/env python3
+
+import argparse
+import subprocess
+import json
+import re
+import config
+
+from slacker import Slacker
+slack = Slacker(config.token)
+
+option_age = ""
+option_owner = None
+option_protocol = 'slack'
+option_ssm = None
+
+query_cache = {}
+HOST="openbmc.gerrit"
+
+
+def query(*args):
+
+    COMMAND = """gerrit query \
+    --format json --all-reviewers \
+    --dependencies --current-patch-set -- \
+    '%s'""" % " ".join(args)
+
+    s = subprocess.Popen(["ssh", "%s" % HOST, COMMAND], 
+                        shell=False,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE)
+
+
+    results = list(map(json.loads, s.stdout.read().splitlines()))
+    del results[-1]
+
+    for r in results:
+        query_cache[r['id']] = r
+
+    return results
+
+
+def changes():
+    args = "age:{0}".format(option_age)
+    if option_owner:
+        args += " ( {0} )".format(option_owner)
+    return query(args,
+                 "status:open", "-is:draft", "-label:Code-Review=-2",
+                 "-project:openbmc/openbmc-test-automation")
+
+
+def change_by_id(change_id):
+    if change_id in query_cache:
+        return query_cache[change_id]
+    c = query(change_id)
+    if len(c):
+        return c[0]
+    return None
+
+
+username_map = {
+    'irc': {
+        'jenkins-openbmc': "Jenkins",
+        'williamspatrick': "stwcx",
+    },
+    'slack': {
+        'adamliyi': "@shyili",
+        'amboar': "@arj",
+        'anoo1': "@anoo",
+        'bradbishop': "@bradleyb",
+        'bjwyman': "@v2cib530",
+        'cbostic': "@cbostic",
+        'chinaridinesh': "@chinari",
+        'dhruvibm': "@dhruvaraj",
+        'dkodihal': "@dkodihal",
+        'devenrao': "@devenrao",
+        'geissonator': "@andrewg",
+        'eddiejames': "@eajames",
+        'gtmills': "@gmills",
+        'jenkins-openbmc': "Jenkins",
+        'JoshDKing': "@jdking",
+        'lgon':"@lgonzalez",
+        'mine260309': "@shyulei",
+        'msbarth': "@msbarth",
+        'mtritz': "@mtritz",
+        'ojayanth': "@ojayanth",
+        'ratagupt': "@ratagupt",
+        'saqibkh': "@khansa",
+        'shenki': "@jms",
+        'spinler': "@spinler",
+        'tomjoseph83': "@tomjoseph",
+        'vishwabmc': "@vishwanath",
+        'williamspatrick': "@iawillia",
+    },
+}
+
+
+def map_username(user):
+    return username_map[option_protocol].get(
+        user[0], "[{0}: {1}]".format(user[0].encode('utf-8'), user[1].encode('utf-8')))
+
+
+def map_approvals(approvals, owner):
+    mapped = {}
+    for a in approvals:
+        approval_type = a['type']
+        approval_owner = (a['by']['username'], a['by']['name'])
+        approval_score = int(a['value'])
+
+        if approval_type not in mapped:
+            mapped[approval_type] = {}
+
+        # Don't allow the owner to self-+1 on code-reviews.
+        if approval_type == 'Code-Review' and approval_owner == owner and \
+                approval_score > 0:
+            continue
+
+        mapped[approval_type][approval_owner] = approval_score
+
+    return mapped
+
+
+def map_reviewers(reviewers, owner):
+    mapped = []
+    for r in reviewers:
+        if 'username' in r:
+            reviewer_user = r['username']
+        else:
+            reviewer_user = "Anonymous-User"
+
+        if 'name' in r:
+            reviewer_name = r['name']
+        else:
+            reviewer_name = "Anonymous Coward"
+
+        if reviewer_user == 'jenkins-openbmc':
+            continue
+
+        reviewer_username = (reviewer_user, reviewer_name)
+
+        if reviewer_user == owner[0]:
+            continue
+
+        mapped.append(reviewer_username)
+
+    return mapped
+
+
+def reason(change):
+    subject = change['subject']
+    owner = (change['owner']['username'], change['owner']['name'])
+    if 'allReviewers' in change:
+        reviewers = map_reviewers(change['allReviewers'], owner)
+    else:
+        reviewers = []
+    if 'approvals' in change['currentPatchSet']:
+        approvals = map_approvals(change['currentPatchSet']['approvals'], owner)
+    else:
+        approvals = {}
+
+    if len(reviewers) < 2:
+        return ("{0} has added insufficient reviewers.", [owner], None)
+
+    if ('Verified' in approvals):
+        verified = approvals['Verified']
+        scores = list(filter(lambda x: verified[x] < 0, verified))
+        if len(scores):
+            return ("{0} should resolve verification failure.", [owner], None)
+
+    if ('Code-Review' not in approvals):
+        return ("Missing code review by {0}.", reviewers, None)
+
+    reviewed = approvals['Code-Review']
+    rejected_by = list(filter(lambda x: reviewed[x] < 0, reviewed))
+    if len(rejected_by):
+        return ("{0} should resolve code review comments.", [owner], None)
+
+    reviewed_by = list(filter(lambda x: reviewed[x] > 0, reviewed))
+    if len(reviewed_by) < 2:
+        return ("Missing code review by {0}.",
+                set(reviewers) - set(reviewed_by), None)
+
+    if ('Verified' not in approvals):
+        return ("May be missing Jenkins verification ({0}).", [owner], None)
+
+    if ('dependsOn' in change) and (len(change['dependsOn'])):
+        for dep in change['dependsOn']:
+            if not dep['isCurrentPatchSet']:
+                return ("Depends on out of date patch set {1} ({0}).",
+                        [owner], dep['id'])
+            dep_info = change_by_id(dep['id'])
+            if not dep_info:
+                continue
+            if dep_info['status'] != "MERGED":
+                return ("Depends on unmerged patch set {1} ({0}).",
+                        [owner], dep['id'])
+
+    approved_by = list(filter(lambda x: reviewed[x] == 2, reviewed))
+    if len(approved_by):
+        return ("Ready for merge by {0}.", approved_by, None)
+    else:
+        return ("Awaiting merge review.", [], None)
+
+send_to_slack = ['@andrewg', 
+                '@bradleyb',
+                '@cbostic', 
+                '@spinler', 
+                '@vishwanath', 
+                '@anoo', 
+                '@dhruvaraj', 
+                '@lgonzalez', 
+                '@gmills', 
+                '@devenrao', 
+                '@dkodihal', 
+                '@chinari', 
+                '@tomjoseph', 
+                '@jdking', 
+                '@eajames', 
+                '@v2cib530', 
+                '@ratagupt',  
+                '@khansa', 
+                '@ojayanth', 
+                '@msbarth']
+
+def do_report(args):
+    action_list = {}
+    for c in changes():
+        print("{0} - {1}".format(c['url'], c['id']))
+        print(c['subject'].encode('utf-8'))
+        (r, people, dep) = reason(c)
+        people = ", ".join(map(map_username, people))
+        print(r.format(people, dep))
+        print("----")
+
+        plist =  people.split(",")
+        for p in plist:
+            p = p.strip()
+            message = "{0} - {1}".format(c['url'], c['id'].encode('utf-8')) 
+            message = message + "\n" + c['subject'].encode('utf-8') + "\n" + r.format(people, dep) + "\n----"
+            action_list.setdefault(p, []).append(message)
+
+    for slack_name, action_description in action_list.iteritems():
+        print "~~~~"
+        print slack_name
+
+        for description in action_description:
+            if option_ssm and slack_name in send_to_slack:
+                print description
+                slack.chat.post_message(slack_name, description)
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--age', help='Change age since last modified', type=str,
+                    default="1d")
+parser.add_argument('--owner', help='Change owner', type=str,
+                    action='append')
+parser.add_argument('--protocol', help='Protocol for username conversion',
+                    type=str, choices=(username_map.keys()))
+parser.add_argument('--sm', action='store_true',help='send slack message flag')
+subparsers = parser.add_subparsers()
+
+report = subparsers.add_parser('report', help='Generate report')
+report.set_defaults(func=do_report)
+
+args = parser.parse_args()
+
+if 'age' in args:
+    option_age = args.age
+if ('owner' in args) and args.owner:
+    option_owner = " OR ".join(map(lambda x: "owner:" + x,
+                                   args.owner))
+if 'protocol' in args and args.protocol:
+    option_protocol = args.protocol
+if args.sm:
+    option_ssm = 'True'
+    print("will send messages to slack")
+else:
+    print("no slack messges will be sent")
+
+if 'func' in args:
+    args.func(args)
+else:
+    parser.print_help()
